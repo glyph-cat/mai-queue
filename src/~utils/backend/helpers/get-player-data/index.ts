a85d6df548b99eb9da64736967aee0be59c6e935
glyph-cat <b75ggnacj@relay.firefox.com>
2023-04-13 20:56:33 +0800

    Switched from puppeteer to playwright

--- src/~utils/backend/helpers/get-player-data/index.ts
+++ src/~utils/backend/helpers/get-player-data/index.ts
@@ -1,57 +1,16 @@
 import { IS_DEBUG_ENV, pickRandom } from '@glyph-cat/swiss-army-knife'
-import chrome from 'chrome-aws-lambda'
 import { DateTime } from 'luxon'
-import puppeteer, {
-  BrowserConnectOptions,
-  BrowserLaunchArgumentOptions,
-  EventEmitter,
-  LaunchOptions,
-} from 'puppeteer'
+import { ConsoleMessage, chromium } from 'playwright'
 import queryString from 'query-string'
-import { DateTimeFormat, ENV, FIREBASE_STORAGE_BASE_URL, StorageBucketNames } from '~constants'
+import {
+  DateTimeFormat,
+  ENV,
+  FIREBASE_STORAGE_BASE_URL,
+  StorageBucketNames,
+} from '~constants'
 import { InvalidFriendCodeError } from '~errors'
 import { STORAGE_BUCKET } from '~services/firebase-admin'
 import { sanitizePlayerName } from '~utils/sanitize-player-name'
-
-type PuppeteerLaunchOptions = LaunchOptions & BrowserLaunchArgumentOptions & BrowserConnectOptions
-
-/**
- * @see https://github.com/johnpolacek/nextjs-scraper-playground
- */
-async function getOptions(): Promise<PuppeteerLaunchOptions> {
-  let environmentBasedOptions: PuppeteerLaunchOptions
-  if (ENV.VERCEL_ENV !== 'localhost') {
-    environmentBasedOptions = {
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-    }
-  } else {
-    environmentBasedOptions = {
-      executablePath: `${process.cwd()}/binaries/Chromium.app/Contents/MacOS/Chromium`,
-    }
-    // environmentBasedOptions = {
-    //   executablePath: process.platform === 'win32'
-    //     ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-    //     : process.platform === 'linux'
-    //       ? '/usr/bin/google-chrome'
-    //       : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    //   headless: true,
-    // }
-  }
-  return {
-    ...environmentBasedOptions,
-    headless: false,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-    ],
-    defaultViewport: {
-      height: pickRandom([600, 900, 675, 720, 900]),
-      width: pickRandom([800, 1000, 1200, 1280, 1600]),
-    },
-  }
-}
 
 export interface PlayerDataObject {
   bannerUrl: string
@@ -62,29 +21,45 @@ export async function getPlayerData(
   friendCode: string
 ): Promise<PlayerDataObject> {
 
-  const options = await getOptions()
-  const browser = await puppeteer.launch(options)
-  const page = await browser.newPage()
-  let debugListener: EventEmitter
+  const browser = await chromium.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  })
+  const context = await browser.newContext({
+    userAgent: 'Mozilla/5.0 (iPad; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1',
+    viewport: {
+      height: pickRandom([600, 900, 675, 720, 900]),
+      width: pickRandom([800, 1000, 1200, 1280, 1600]),
+    },
+  })
+  const page = await context.newPage()
+
+  let onConsole: (msg: ConsoleMessage) => Promise<void>
   if (IS_DEBUG_ENV) {
-    debugListener = page.on('console', async (msg) => {
+    onConsole = async (msg: ConsoleMessage) => {
       const msgArgs = msg.args()
       for (let i = 0; i < msgArgs.length; ++i) {
         // eslint-disable-next-line no-console
         console.log(await msgArgs[i].jsonValue())
       }
-    })
+    }
+    page.on('console', onConsole)
   }
 
   // Login
   const loginUrl = 'https://lng-tgk-aime-gw.am-all.net/common_auth/login?site_id=maimaidxex&redirect_url=https://maimaidx-eng.com/maimai-mobile/&back_url=https://maimai.sega.com/'
-  await page.goto(loginUrl, { waitUntil: 'networkidle0' })
+  await page.goto(loginUrl, { waitUntil: 'networkidle' })
   await page.click('span.c-button--openid--segaId')
   await page.type('#sid', ENV.SEGA_ID)
   await page.type('#password', ENV.SEGA_PW)
   await Promise.all([
     page.click('#btnSubmit'),
-    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    // page.waitForSelector('img[src="https://maimaidx-eng.com/maimai-mobile/img/menu_friend.png"]'),
+    // body > div.wrapper.main_wrapper.t_c > header > div > div:nth-child(2) > a:nth-child(3) > img
+    page.waitForNavigation({ waitUntil: 'networkidle' }),
   ])
 
   // Get player data
@@ -92,7 +67,7 @@ export async function getPlayerData(
     url: 'https://maimaidx-eng.com/maimai-mobile/friend/search/searchUser',
     query: { friendCode },
   })
-  await page.goto(friendSearchUrl, { waitUntil: 'networkidle0' })
+  await page.goto(friendSearchUrl, { waitUntil: 'networkidle' })
 
   const bannerSelector = 'body > div.wrapper.main_wrapper.t_c > div.see_through_block.m_15.m_t_5.p_10.t_l.f_0.p_r > div.basic_block.p_10.f_0'
   try {
@@ -122,8 +97,8 @@ export async function getPlayerData(
     bannerElement.style.boxShadow = 'none'
   }, bannerSelector)
   const screenshot = (await userBanner.screenshot({
-    encoding: 'binary',
     omitBackground: true,
+    type: 'png',
   })) as string | Buffer
   const screenshotName = `${friendCode}-${DateTime.now().toFormat(DateTimeFormat.SCREENSHOT_DATE_TIME)}.png`
   const file = STORAGE_BUCKET.file(`${StorageBucketNames.PlayerBannerScreenshots}/${screenshotName}`)
@@ -147,9 +122,9 @@ export async function getPlayerData(
     return nameElement?.innerText || null
   }))
 
+  if (IS_DEBUG_ENV) { page.removeListener('console', onConsole) }
   await page.close()
   await browser.close()
-  if (debugListener) { debugListener.removeAllListeners() }
 
   return {
     bannerUrl,
